c7c84ad74b7583922dc7d0dbc60a25ca83aa0cd7
SuperVK <victor.klomp@gmail.com>
2020-09-27 19:35:02 +0200

    updated colors and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
 	render() {
 		return (
-			<Html style={{ background: '#EEE', color: '#444' }}>
+			<Html style={{ background: '#ffffff', color: '#4527A0' }}>
 				<Head>
 
 					<link rel="manifest" href="/manifest.json" />
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
 						href="https://code.getmdl.io/1.3.0/material.deep_purple-blue.min.css"
 					/>
 					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css"></link>
-                    <meta name='theme-color' content='#000000'/>
+                    <meta name='theme-color' content='#4527A0'/>
 				</Head>
 				<body>
 					<Main />
