ef665d379a671be3cc5d874c110eebb3ce1b7828
SuperVK <victor.klomp@gmail.com>
2020-09-28 23:38:01 +0200

    changed colors

--- components/Flashcards/DutchTranslation.js
+++ components/Flashcards/DutchTranslation.js
@@ -3,11 +3,11 @@ const { Typography, Divider } = require("@material-ui/core");
 const DutchTranslation = ({ dutchTranslation, userInput, visibility }) => (
 	visibility == 'COMPARE' 
 	? <div>
-		{dutchTranslation}
+		{userInput}
 		<div style={{height: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}><Divider></Divider></div>
-		<span style={{color: '#400189'}}>{userInput}</span>
+		<span style={{color: '#400189'}}>{dutchTranslation}</span>
 	</div>
-	: <div>{dutchTranslation}</div>
+	: <div style={{color: '#400189'}}>{dutchTranslation}</div>
 )	
 
 export default DutchTranslation
