dd53337a83ceb552ad7141da4ee694734b2448f0
SuperVK <victor.klomp@gmail.com>
2020-10-01 21:35:08 +0200

    fixed multiline

--- components/Flashcards/DutchInput.js
+++ components/Flashcards/DutchInput.js
@@ -2,6 +2,6 @@ import { TextField } from '@material-ui/core'
 import styles from '../../styles/DutchInput.module.css'
 
 const DutchInput = ({ value, onChange }) => (
-	<TextField className={styles.DutchInput} id="outlined-basic" label="Vul de Nederlandse text in...." variant="outlined" value={value} onChange={e => onChange(e.target.value)} />
+	<TextField multiline autoFocus className={styles.DutchInput} id="outlined-basic" label="Vul de Nederlandse text in...." variant="outlined" value={value} onChange={e => onChange(e.target.value)} />
 )
 export default DutchInput
